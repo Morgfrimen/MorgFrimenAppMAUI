1fa5347b6fab46add446fa5c56eb861cde313a3f
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-09-26 13:23:40 +0300

    Change propertychanged

--- BlazorWebAssembly/ViewModels/Adstract/BaceViewModels.cs
+++ BlazorWebAssembly/ViewModels/Adstract/BaceViewModels.cs
@@ -1,10 +1,20 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorWebAssembly.ViewModels.Adstract
 {
-	public abstract class BaceViewModels : INotifyPropertyChanged, IDisposable
+	public abstract class BaceViewModels : ComponentBase, INotifyPropertyChanged, IDisposable
 	{
+		public BaceViewModels()
+		{
+			PropertyChanged+=async (sender,e) => await InvokeAsync(() =>
+			{
+				StateHasChanged();
+			});
+		}
+		
 		public event PropertyChangedEventHandler? PropertyChanged;
 		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
